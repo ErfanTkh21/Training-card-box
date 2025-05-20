74cd3b1128936a1c45347383278195bab69d7b88
Erfan <erfantkh20073@gmail.com>
2025-05-20 10:37:07 +0330

    build the card boxes

--- js/app.js
+++ js/app.js
@@ -4,9 +4,7 @@ const submitButton = document.querySelector(".submit-btn");
 
 ratingButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    // حذف انتخاب قبلی
     ratingButtons.forEach((b) => b.classList.remove("selected"));
-    // علامت‌گذاری دکمه فعلی
     btn.classList.add("selected");
     selectedRating = btn.textContent;
   });
